bd294fa11048405c2604ff3dc3ae006044efb2f7
etiennebch <et.bouche@gmail.com>
2016-11-15 20:53:32 +0100

    ex06, added a missing condition on *(s2 +i != 0)

--- ex06/ft_strcmp.c
+++ ex06/ft_strcmp.c
@@ -7,14 +7,14 @@ int	ft_strcmp(char *s1, char *s2)
 	i = 0;
 	flag = 0;
 	result = 0;
-	while (*(s1 + i) != 0 && flag == 0)
+	while (*(s1 + i) != 0 && *(s2 + i) != 0 && flag == 0)
 	{
 		result = *(s1 + i) - *(s2 + i);
 		if (result != 0)
 			flag = 1;
 		i++;
 	}
-	if (*(s1 + i) == 0)
+	if (*(s1 + i) == 0 || *(s2 + i) == 0)
 		result = *(s1 + i) - *(s2 + i);
 	return (result);
 }
