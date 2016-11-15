b2fcfe4236c974f6153111043fccce3b2d1de4d5
etiennebch <et.bouche@gmail.com>
2016-11-15 19:47:55 +0100

    ex06

---
+++ ex06/ft_strcmp.c
@@ -0,0 +1,20 @@
+int	ft_strcmp(char *s1, char *s2)
+{
+	unsigned int	i;
+	char		flag;
+	int		result;
+
+	i = 0;
+	flag = 0;
+	result = 0;
+	while (*(s1 + i) != 0 && flag == 0)
+	{
+		result = *(s1 + i) - *(s2 + i);
+		if (result != 0)
+			flag = 1;
+		i++;
+	}
+	if (*(s1 + i) == 0)
+		result = *(s1 + i) - *(s2 + i);
+	return (result);
+}
